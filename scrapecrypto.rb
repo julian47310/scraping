670eae1e5e9c0c0ed219c123a0275278ebdada01
julian <julian47310@gmail.com>
2020-04-16 23:52:57 +0200

    Update scrapecrypto.rb

--- scrapecrypto.rb
+++ scrapecrypto.rb
@@ -3,11 +3,16 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all"))
-a = []
-hash = {}
-for i in 1..200
-	
-	a << hash={page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[3]/div").text => page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[5]/a").text.gsub(/[^\d\.]/, '').to_f}
+
+
+
+def cryptoarrayhash
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all"))
+	hash = {}
+	a = []
+	for i in 1..200
+		a << hash={page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[3]/div").text => page.xpath("/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[5]/a").text.gsub(/[^\d\.]/, '').to_f}
+	end
+	print a
 end
-print a
+cryptoarrayhash
